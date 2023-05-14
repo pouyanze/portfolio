8371b4559f9a4f9771c4d02b73af3a59e3619a06
pouyan zeraati chaveshlou <pouyan.ze@gmail.com>
2023-05-14 12:15:50 -0600

    add blog model

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    body = models.TextField()
+    image = models.ImageField(upload_to='posts', blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
-# Create your models here.
+    def __str__(self):
+        return str(self.title)
