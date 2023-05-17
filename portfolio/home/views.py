87737a0f6138c44fce3436f04f20fdbde97e5a75
pouyan zeraati chaveshlou <pouyan.ze@gmail.com>
2023-05-17 16:05:37 -0600

    change index view fun to class based

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView 
+from blog.models import Post
+from work_gallery.models import Work
+class HomePageView(TemplateView):
+    template_name = 'home/index.html'
 
-def index(request):
-    return render(request, 'home/index.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['work_experiences'] = Work.objects.order_by('-date')[:2]
+        context['posts'] = Post.objects.order_by('-last_modified')[:2]
+        return context
